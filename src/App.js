f1ab805b608c0b76c02f4c96ba48ed1b7e90c429
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-26 18:04:35 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a
+          href="https://www.linkedin.com/in/fernanda-gomes-front-end-developer/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Fernanda Gomes
+        </a>
+        {""}
+        and is open-source on{" "}
+        <a
+          href="https://github.com/FernandaSGomes/my-react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
