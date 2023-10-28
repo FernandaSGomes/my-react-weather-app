35823f3247ada0d1b5b8c7b92349ee088c888363
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-28 10:56:38 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://www.linkedin.com/in/fernanda-gomes-front-end-developer/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Fernanda Gomes
-        </a>
-        {""}
-        and is open-source on{" "}
-        <a
-          href="https://github.com/FernandaSGomes/my-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/fernanda-gomes-front-end-developer/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Fernanda Gomes
+          </a>{" "}
+          and is open-source on{" "}
+          <a
+            href="https://github.com/FernandaSGomes/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
