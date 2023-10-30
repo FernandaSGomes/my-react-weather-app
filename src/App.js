7d8a80b385fb1a3489b6c91f5284a9d892b31ac8
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-30 20:08:51 +0000

    Added Netlify link

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ export default function App() {
             rel="noreferrer"
           >
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://current-weather-point.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
