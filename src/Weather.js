299b894d2bf9f238a7b6bf5fd860f12bb583d22c
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-30 20:35:27 +0000

    Added background image

--- src/Weather.js
+++ src/Weather.js
@@ -37,7 +37,7 @@ export default function Weather(props) {
 
   if (weatherData.ready) {
     return (
-      <div className="Weather">
+      <div className="Weather shadow br-3">
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
