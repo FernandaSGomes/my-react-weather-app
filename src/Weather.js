9ebc9e73936c53dc42547638054bd57918b2b064
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-30 20:39:05 +0000

    Update css

--- src/Weather.js
+++ src/Weather.js
@@ -37,14 +37,14 @@ export default function Weather(props) {
 
   if (weatherData.ready) {
     return (
-      <div className="Weather shadow br-3">
+      <div className="Weather shadow ">
         <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city.."
-                className="form-control"
+                className="form-control shadow"
                 autoFocus="on"
                 onChange={handleCityChange}
               />
@@ -53,7 +53,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-primary w-100 shadow"
               />
             </div>
           </div>
